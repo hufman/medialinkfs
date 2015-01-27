c4902272f3f2fc155f4c569e5946a5f5918f394b
Walter Huf <hufman@gmail.com>
2015-01-26 21:53:33 -0600

    Disables the freebase plugin from tests
    Freebase's free tier has gotten really small, so the tests fail in it

--- tests.sh
+++ tests.sh
@@ -9,6 +9,7 @@ for i in tests/*.py; do
 	testname=`basename $i .py`
 	echo "$testname" | grep "^_" > /dev/null && continue
 	echo "$testname" | grep "id3" > /dev/null && continue
+	echo "$testname" | grep "freebase" > /dev/null && continue
 	echo "Running tests in $testname"
 	python3 -m unittest tests.$testname || failed=1
 	echo
