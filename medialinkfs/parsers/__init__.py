0bbdcac178a0c46e8c248bf9ccdf312e41f49f13
Walter Huf <hufman@gmail.com>
2015-10-29 19:54:17 -0500

    Adds a quick hack to support module aliasing
    For dummy module testing, we can now have multiple dummy parser
    with their own parser_options

--- medialinkfs/parsers/__init__.py
+++ medialinkfs/parsers/__init__.py
@@ -1,4 +1,8 @@
 import importlib
 def load_parser(parser_name, parser_options):
+	# strip off aliases
+	parser_name = parser_name.split('.')[0]
+	# load the module
 	module = importlib.import_module('.'+parser_name, 'medialinkfs.parsers')
+	# instantiate the module with the options
 	return module.Module(parser_options)
